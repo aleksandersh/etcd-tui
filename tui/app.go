e9b0d37a06e28401de76cad82fe0419e0fd17ec0
aleksandersh <alex-shuttle@yandex.ru>
2024-06-06 21:35:42 +0200

    update styles

--- tui/app.go
+++ tui/app.go
@@ -6,10 +6,14 @@ import (
 
 	"github.com/aleksandersh/etcd-tui/data"
 	"github.com/aleksandersh/etcd-tui/domain"
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func RunApp(ctx context.Context, config *domain.Config, dataSource *data.EtcdDataSource, list *domain.EntityList) error {
+	tview.Styles.TertiaryTextColor = tcell.ColorDarkOrange
+	tview.Styles.ContrastBackgroundColor = tcell.ColorDarkSlateGray
+
 	app := tview.NewApplication()
 
 	pagesView := tview.NewPages()
